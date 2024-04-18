41ac6d5b78e6ca1036865cf0610f83528a0852b9
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-04-18 18:57:52 +0300

    Update test_nikto.py

--- additionalTask_1/test_nikto.py
+++ additionalTask_1/test_nikto.py
@@ -1,1 +1,15 @@
+import subprocess
+import pytest
 
+@pytest.mark.nikto
+def test_nikto_output():
+    # Выполнение команды nikto
+    command = "nikto -h https://test-stand.gb.ru/ -ssl -Tuning 4"
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    # Проверка наличия текста "0 error(s)" в выводе команды
+    assert "0 error(s)" in result.stdout, f"Expected '0 error(s)' not found in output:\n{result.stdout}"
+
+    # Вывод результатов команды
+    print("Nikto command output:")
+    print(result.stdout)
